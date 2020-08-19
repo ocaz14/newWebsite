4bc736956751130dd41f758f636d6c74b41f33f8
ocaz14 <nasri.anas14@gmail.com>
2020-08-19 12:04:44 +0800

    melek matematika declaration

---
+++ project/melek-matematika/js/mm-home.js
@@ -0,0 +1,119 @@
+var navContainer = [];
+for (i = 0; i < titleBlogCenter.length; i++) {
+  navContainer[i] =
+    `<li><a href='` + URLBlog[i] + `'>` + titleBlogCenter[i] + `</a></li>`;
+}
+
+//side nav
+var sideNav =
+  "<div id='sideSection-src' class='side-section'>" +
+  "<div class='top-line'></div>" +
+  //project
+  "<div class='sub-side-section'>" +
+  "<h3><a href='/project.html'>Project</a></h3>" +
+  "</div>" +
+  //coding
+  "<div class='sub-side-section'>" +
+  "<h3><a href='/coding.html'>Coding</a></h3>" +
+  "</div>" +
+  //blog
+  "<div class='sub-side-section'>" +
+  "<h3><a href='/blog.html'>Blog</a></h3>" +
+  "<ul>" +
+  navContainer.join("") +
+  "</ul>" +
+  "</div>" +
+  //Game
+  "<div class='sub-side-section'>" +
+  "<h3> <a href='/game.html'>Game</a></h3>" +
+  "</div>" +
+  "</div>";
+
+//default
+var pageChoice = 1;
+
+var pageActive = document.getElementsByClassName("page-active")[0].id;
+console.log(pageActive);
+
+var titlePageActive;
+var dateBlogActive;
+for (i = 0; i < idBlog.length; i++) {
+  if (pageActive === idBlog[i]) {
+    titlePageActive = titleBlogCenter[i];
+    dateBlogActive = dateBlog[i];
+  }
+}
+
+if (pageActive) {
+  document.getElementById("Blogtitle").innerText = titlePageActive;
+  document.getElementById(pageActive).innerText = titlePageActive;
+  document.title = "Blog: " + titlePageActive;
+  document.getElementById("dateBlog").innerText = dateBlogActive;
+  //document.getElementById("meta-keywords").content = "Keywords?";
+} else {
+  page(pageChoice);
+}
+
+document.getElementById("sideNav-section").innerHTML = sideNav;
+
+function page(pageChoice) {
+  var groupPost;
+  var totalPost = 3;
+  var pageActive = pageChoice;
+
+  console.log(pageActive);
+  var maxPage = Math.ceil(titleBlogCenter.length / totalPost);
+  for (i = 0; i < maxPage; i++) {
+    var equation = i + 1;
+    if (pageActive == equation) {
+      groupPost = equation * totalPost;
+    }
+  }
+  var summaryStart = titleBlogCenter.length - groupPost;
+  var pageDiplay;
+  if (titleBlogCenter.length - summaryStart < totalPost) {
+    pageDiplay = titleBlogCenter.length - summaryStart;
+  } else {
+    pageDiplay = totalPost;
+  }
+  var summaryContainer = [];
+  for (i = 0; i < pageDiplay; i++) {
+    var page = i + summaryStart;
+    summaryContainer[page] =
+      `<a class='summary-page-title' href='` +
+      URLBlog[page] +
+      `'><h2>` +
+      titleBlogCenter[page] +
+      `</h2><h6>` +
+      dateBlog[page] +
+      `</h6><p>` +
+      descriptionBlog[page] +
+      `</p></a>`;
+  }
+  summaryContainer.reverse();
+  var summary =
+    "<div class='summary-title-center'><h1>Blog Area</h1>" +
+    "<h5>Page :  " +
+    pageActive +
+    " / " +
+    maxPage +
+    "</h5></div>" +
+    "<div id='numberNavs2' class='numberNavs'></div>" +
+    summaryContainer.join("");
+
+  var numberNav = [];
+  for (i = 0; i < maxPage; i++) {
+    var num = i + 1;
+    numberNav[i] =
+      "<div class='numberNav' onclick='page(" + num + ")'>" + num + "</div>";
+  }
+
+  var numberNavContainer =
+    "<div class='arrowNav' onclick=''><</div>" +
+    numberNav.join("") +
+    "<div class='arrowNav' onclick=''>></div>";
+
+  document.getElementById("summary").innerHTML = summary;
+  document.getElementById("numberNavs").innerHTML = numberNavContainer;
+  document.getElementById("numberNavs2").innerHTML = numberNavContainer;
+}
---
+++ project/melek-matematika/js/mm-input.js
@@ -0,0 +1,40 @@
+//ID
+var idBlog = [
+  "linkBlog_2020_08_4",
+  "linkBlog_2020_08_5",
+
+  "linkBlog_2020_08_6",
+  "linkBlog_2020_08_7",
+];
+//URL
+var URLBlog = [
+  "/blog/2020/08/cara-menjumlahkan-pecahan",
+  "/blog/2020/08/cara-mengubah-pecahan-campuran-menjadi-pecahan-biasa",
+
+  "/blog/2020/08/cara-mengubah-pecahan-biasa-ke-bentuk-persen",
+  "/blog/2020/08/cara-mengubah-pecahan-biasa-menjadi-pecahan-campuran",
+];
+//title
+var titleBlogCenter = [
+  "Beberapa yang Bisa Digunakan untuk Menyelesaikan Penjumlahan Pecahan",
+  "Cara Mengubah Pecahan Campuran menjadi Pecahan Biasa",
+
+  "Cara Mengubah Pecahan Biasa ke Bentuk Persen",
+  "Cara Mengubah Pecahan Biasa menjadi Pecahan Campuran",
+];
+//date
+var dateBlog = [
+  "August 15, 2020",
+  "August 16, 2020",
+
+  "August 17, 2020",
+  "August 18, 2020",
+];
+
+var descriptionBlog = [
+  "Pecahan adalah bentuk lain dari bilangan desimal. misalnya 1/4 adalah bentuk lain dari 0.25. Operasi tambah dan kurang pecahan berbeda dengan kali dan bagi. Operasi tambah dan kurang pecahan membutuhkan sedikit perhatian untuk diselesaikan. Berikut ini beberapa cara yang bisa anda gunakan dalam menjumlahkan dan mengurangkan pecahan.",
+  "Pecahan campuran biasanya diubah menjadi pecahan biasa, agar bisa dilakukan proses operasi, baik tambah, kurang, kali, dan bagi. Berikut ini adalah langkah yang bisa dilakukan untuk mengubah pecahan campuran menjadi pecahan biasa. ",
+
+  "Mengubah pecahan campuran menjadi bentuk persen, bisa dilakukan dengan beberapa cara. Berikut ini cara yang paling mudah dipahami beserta tambahan cara cepat dan cara dengan menggunakan mesin hitung.",
+  "Tidak semua pecahan bisa diubah menjadi pecahan campuran. Hanya pecahan yang pembilangnya lebih besar dari penyebut yang bisa diubah menjadi pecahan campuran. jika lebih kecil, maka tidak bisa diubah, atau hasilnya adalah pecahan itu sendiri. Jika besar pembilang dan penyebut sama, maka hasilnya adalah bilangan bulat 1, bukan pecahan campuran. Berikut cara merubah pecahan biasa menjadi pecahan campuran.",
+];
