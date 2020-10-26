d4b9a5953aa2904a384ac4eeff2f9b29706f396a
ocaz14 <nasri.anas14@gmail.com>
2020-10-26 15:45:24 +0800

    up

--- blog/js/input-blog.js
+++ blog/js/input-blog.js
@@ -6,7 +6,7 @@ var class_category = [
 var Post_List = [
   [
     "linkBlog_2020_03_1",
-    "/blog/2020/03/cara-menghasilkan-uang-dari-internet",
+    "/2020/03/cara-menghasilkan-uang-dari-internet",
     "Cara Menghasilkan Uang dari Intenet",
     "March 20, 2020",
     "Virus Corona atau Covid-19 atau coronavirus Akhir-akhir ini cukup merepotkan. Bahkan hampir diseluruh dunia mengharuskan untuk tetap dirumah agar penyakit tersebut tidak semakin menyebar. Berdiam diri dirumah untuk beberapa orang adalah hal yang sangat merugikan. Maka dari itu akan sangat membahagiakan jika dapat menghasilkan uang dari rumah. Ada beberapa cara memperoleh uang secara online hari ini. Termasuk yang sifatnya jangka panjang. Menghasilkan uang secara online keliatannya cukup susah, tapi sebenarnya hanya membutuhkan sedikit kedisiplinan.",
@@ -15,7 +15,7 @@ var Post_List = [
   ],
   [
     "linkBlog_2020_03_2",
-    "/blog/2020/03/SEO-membantu-google-memahami-situs-dan-halaman-anda",
+    "/2020/03/SEO-membantu-google-memahami-situs-dan-halaman-anda",
     "SEO: Membantu Google Memahami Situs dan Halaman Anda",
     "March 22, 2020",
     "Google merupakan perusahaan mesin pencari. Yang mana akan melakukan segala hal untuk tetap menjadi perusahaan mesin pencari terbaik dengan memberikan pengalaman yang memuaskan kepada pengguna. Pengalaman memuaskan, misalnya ketika topik yang dicari oleh pengguna dapat langsung ditemukan, dan ditemukannya dalam waktu singkat. Sehingga Google akan menerapkan algoritma untuk itu dimana memunculkan situs yang paling sesuai dengan pencarian tertentu. Dengan SEO yang baik, pemasang situs akan sangat mudah ditemukan pada pencarian Google.",
@@ -24,7 +24,7 @@ var Post_List = [
   ],
   [
     "linkBlog_2020_03_3",
-    "/blog/2020/03/paket-murah-telkomsel-maret2020",
+    "/2020/03/paket-murah-telkomsel-maret2020",
     "Paket Murah Telkomsel - Maret 2020",
     "March 25, 2020",
     "Coronavirus melanda dunia, hampir semua negara mengisyaratkan untuk kerja dirumah tidak terkecuali pemerintah Indonesia. Yang mana menandakan jumlah kaum rebahan akan meningkat untuk beberapa minggu kedepan. Belum lagi virus baru yaitu Hantavirus yang numpang tenar. Pasokan utama kaum rebahan bukan sembako, bukan masker, bukan Hand Sanitizer. Tak lain dan tak bukan adalah paket Internet. Paket internet cukup banyak di luar sana. Banyak macam, tetapi beberapa kaum rebahan lebih memilih bersama Telkomsel mesikpun paketnya lebih sering mengkhianati. Tunggu dulu, siapa bilang semua paket internet Telkomsel itu mahal. Berikut ini beberapa paket Telkomsel turunan *363# yang kami rangkum untuk anda.",
@@ -33,7 +33,7 @@ var Post_List = [
   ],
   [
     "linkBlog_2020_03_4",
-    "/blog/2020/03/cara-mengoptimalkan-konten-website-anda",
+    "/2020/03/cara-mengoptimalkan-konten-website-anda",
     "Cara Mengoptimalkan Konten Website Anda",
     "March 28, 2020",
     "Selain hal teknis yang dijelaskan untuk Membantu Google Memahami Situs dan Halaman Anda, beberapa hal berikut juga tidak kalah berpengaruh untuk membuat situs anda berada di perangkat teratas di mesin pencari. Mulai dari situs yang menarik dan bermanfaat, mudah dibaca, memanjakan pengguna dalam menghadapi mesin pencari, konten yang unik dan rapi, membangun kepercayaan pengguna dan mesin pencari, penempatan iklan yang baik, penggunaan link yang baik, dan bagaimana google menemukan gambar pada mesin pencari.",
@@ -42,7 +42,7 @@ var Post_List = [
   ],
   [
     "linkBlog_2020_03_5",
-    "/blog/2020/03/pentingnya-rutinitas",
+    "/2020/03/pentingnya-rutinitas",
     "Pentingnya Rutinitas",
     "March 29, 2020",
     "Rutinitas adalah pekerjaan yang dilakukan setiap hari secara berulang dengan tanpa pikir panjang. Misalnya seperti makan, mandi, tidur, beribadah dan lain sebagainya. Anda pasti tidak memikirkan lagi bagaimana anda mengunyah makan, bagaimana anda menelan, bagaimana anda menyuap diri anda sendiri, dan bagaimana perut anda harus mengolah makanannya (tentu saja anda tidak perlu memikirkan itu). Bisa dikata dengan rutinitas anda bekerja dengan otomatis, tanpa arahan, paksaan, dan kemudian anda mencapai tujuan anda. ",
@@ -51,7 +51,7 @@ var Post_List = [
   ],
   [
     "linkBlog_2020_08_1",
-    "/blog/2020/08/cek-pajak-kendaraan-sulsel",
+    "/2020/08/cek-pajak-kendaraan-sulsel",
     "Cara Cek Pajak Kendaraan Bermotor dari rumah",
     "August 11, 2020",
     "Tahukah anda, bahwa total pembayaran yang harus anda bayar untuk pajak kendaraan anda, sudah bisa anda cek dari rumah sebelum ke tempat pembayaran. Hal ini tentu memberikan sedikit kemudahan dibandingkan harus bolak-balik karena dana yang disiapkan tidak mencukupi.",
@@ -60,7 +60,7 @@ var Post_List = [
   ],
   [
     "linkBlog_2020_08_2",
-    "/blog/2020/08/paket-tengah-malam-telkomsel",
+    "/2020/08/paket-tengah-malam-telkomsel",
     "Paket Tengah Malam Telkomsel",
     "August 12, 2020",
     "Tahukah anda bahwa Telkomsel juga punya paket data malam. Harganya pun terbilang cukup murah. Harga berkisar antara Rp 2.000,- hingga Rp 22.000,- untuk paket data antara 2GB hingga 15GB menjadikannya sangat terjangkau untuk semua kalangan. Pada dial ini terdapat beberapa pilihan paket data yang bisa anda sesuaikan dengan kebutuhan anda. Paket bulanan, mingguan, hingga harian tersedia disini. Salah satunya yaitu 15GB/30hari yang hanya Rp. 22.000,-.",
@@ -69,7 +69,7 @@ var Post_List = [
   ],
   [
     "linkBlog_2020_08_3",
-    "/blog/2020/08/syarat-minimum-google-adsense",
+    "/2020/08/syarat-minimum-google-adsense",
     "Syarat Minimum Google Adsense",
     "August 13, 2020",
     "Mengapa membahas mengenai diterima Google Adsense menjadi penting? karena tidak sedikit penyedia konten online yang 'ditolak' untuk menjadi anggota penampil iklan Google Adsense. Beberapa bahkan harus mencoba beberapa kali. Maka dari itu, perlu untuk diketahui mengenai apa sebenarnya syarat minimal untuk dapat diterima menjadi anggota Goggle Adsense",
@@ -78,7 +78,7 @@ var Post_List = [
   ],
   [
     "linkBlog_2020_10_1",
-    "/blog/2020/10/paket-internet-murah-telkomsel-2020",
+    "/2020/10/paket-internet-murah-telkomsel-2020",
     "Paket Murah Telkomsel Akhir Tahun 2020",
     "October 7, 2020",
     "Di akhir tahun 2020 ini Telkomsel meluncurkan paket Internet yang terbilang cukup murah. Paket Tersebut dinamakan paket Combo Unlimited. Dengan masa berlaku 1 bulan, paket ini memberikan kuota internet 4GB, telpon ke sesama Telkomsel sepuasnya, dan Whatsapp sepuasnya termasuk telpon video. Paket ini dihargai Rp 10.000 (sepuluh ribu rupiah). Murah bukan?",
