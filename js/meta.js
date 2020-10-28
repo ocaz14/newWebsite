852f54167771834b4f6e64954de89bef955bf41f
ocaz14 <nasri.anas14@gmail.com>
2020-10-28 15:56:29 +0800

    up

--- js/meta.js
+++ js/meta.js
@@ -1,2 +1,8 @@
-var meta = document.getElementById("description").innerText;
-document.getElementById("meta-description").content = meta;
+var meta = document.getElementById("description");
+if(meta){
+    meta.innerText;
+    document.getElementById("meta-description").content = meta;
+}
+
+document.getElementById("theme-color").content = " #03dac5";
+document.getElementById("icon").href = "/img/favicon.png";
