38ae99c1e8be56fd2d76d427c54b7148a2f167c9
ocaz14 <nasri.anas14@gmail.com>
2020-11-24 16:37:10 +0800

    list fix

--- project/calculator/js/sidebar-calc.js
+++ project/calculator/js/sidebar-calc.js
@@ -1,5 +1,6 @@
 var pageId = document.getElementsByClassName("page-active")[0].id;
 var pageCategory;
+var cat = class_category_calc;
 
 if(pageId){
     for(var i=0; i<footerArray.length; i++){
@@ -16,7 +17,6 @@ var sidebar = document.getElementById("sidebar");
 var rekArr = [];
 var rekResult = [];
 var jumlah_Category = pageCategory.length;
-
 
 for(var n = 0; n<jumlah_Category; n++){                     //tiap kategori pada halaman terpilih 
     for(var i=0; i<footerArray.length; i++){                //cek kategori pada semua anggota
@@ -39,26 +39,43 @@ function push_noDuplicate(data){
 for(var i=0; i<rekArr.length; i++){
     rekResult[i] = "<a class='"+rekArr[i]+"' href=''></a>";
 }
+
 var rekomendasi_Sidebar = 
 "<h3>REKOMENDASI</h3>"+
 "<ul>"+
 rekResult.join("");
 "</ul>";
 
-//lihat juga sidebar
+
+//Lihat Juga Sidebar
+var catArrAll = [];
+for(var i=0; i< cat.length; i++){                       //semua kategori
+    catArrAll.push(cat[i][0])
+}
+
+var chosenCat = [];
+for(var n = 0; n< catArrAll.length; n++){                   //list dari 
+    var catMember = [];
+    for(var i=0; i<footerArray.length; i++){                //cek kategori pada semua anggota
+        for(var j=0; j<footerArray[i][5].length; j++){      //cek kategori dari anggota
+            if(footerArray[i][5][j] == catArrAll[n]){
+                catMember.push(footerArray[i][0])
+            }
+        }
+    }
+    var member_Random = Math.floor(Math.random() * catMember.length);
+    chosenCat.push(catMember[member_Random]);
+}
+
+var lihatJuga_result = [];
+for(var i=0; i<chosenCat.length; i++){
+    lihatJuga_result[i] = "<a class='"+chosenCat[i]+"' href=''></a>";
+}
+
 var lihatjuga_Sidebar = 
 "<h3>LIHAT JUGA</h3>"+
 "<ul>"+
-"  <a class='Aug20-ProK_01' href=''></a>"+
-"  <a class='June20-ProK_07' href=''></a>"+
-"  <a class='Aug20-ProK_13' href=''></a>"+
-"  <a class='July20-ProK_01' href=''></a>"+
-"  <a class='Aug20-ProK_25' href=''></a>"+
-"  <a class='Aug20-ProK_28' href=''></a>"+
-"  <a class='June20-ProK_01' href=''></a>"+
-"  <a class='June20-ProK_05' href=''></a>"+
-"  <a class='June20-ProK_06' href=''></a>"+
-"  <a class='July20-ProK_02' href=''></a>"+
+lihatJuga_result.join("");
 "</ul>";
 
 sidebar.innerHTML = 
