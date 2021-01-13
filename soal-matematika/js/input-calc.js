183169c9c59adf9ca84d9640c87aa281aa0a62cd
ocaz14 <nasri.anas14@gmail.com>
2021-01-13 19:44:19 +0800

    update

--- soal-matematika/js/input-calc.js
+++ soal-matematika/js/input-calc.js
@@ -6,15 +6,17 @@
 var url_root = "https://nasrianas.website/soal-matematika";
 
 var class_category_calc = [
-  ["Debit", "/waktu", ""],
-  ["Kecepatan", "/waktu", ""],
+  ["Debit", "/debit-air", ""],
+  ["Kecepatan", "/kecepatan", ""],
+  ["Volume", "/volume", ""],
+  ["Skala", "/skala", ""],
 ];
 
 var footerArray = [
   [
     "Jan21_SM_01",
     "/kelas-5/debit-air/001-soal-matematika",
-    "Debit air pada keran kamar mandi 0.5 liter/detik. Volume air dalam bak mandi setelah 4 menit adalah ",
+    "Debit air pada keran kamar mandi 0.5 liter/detik. Volume air dalam bak mandi setelah 4 menit adalah ...",
     ["Debit"],
   ],
   [
@@ -32,8 +34,74 @@ var footerArray = [
   [
     "Jan21_SM_04",
     "/kelas-5/kecepatan/004-soal-matematika",
-    "Tiara bersepeda dari rumah menuju rumah nenek dengan kecepatan 12 km/jam. Jarak rumah Tara dan rumah nenek 1400 meter. Waktu yang diperlukan Tara untuk sampai dirumah nenek adalah",
+    "Tiara bersepeda dari rumah menuju rumah nenek dengan kecepatan 12 km/jam. Jarak rumah Tara dan rumah nenek 1400 meter. Waktu yang diperlukan Tara untuk sampai dirumah nenek adalah ...",
     ["Kecepatan"],
+  ],
+  [
+    "Jan21_SM_05",
+    "/kelas-5/kecepatan/005-soal-matematika",
+    "Tora berlari dengan kecepatan 8 km/jam, jarak yang ditempuh Tora setiap berlari 15 menit adalah ...",
+    ["Kecepatan"],
+  ],
+  [
+    "Jan21_SM_06",
+    "/kelas-5/volume/006-soal-matematika",
+    "15000 Liter = ... meter kubik (m<sup>3</sup>)",
+    ["Volume"],
+  ],
+  [
+    "Jan21_SM_07",
+    "/kelas-5/debit-air/007-soal-matematika",
+    "Air terjun dalam waktu 1 jam mengalirkan air sebanyak 432 m<sup>3</sup>. Debit air terjun tersebut adalah ... dm<sup>3</sup> / detik",
+    ["Debit"],
+  ],
+  [
+    "Jan21_SM_08",
+    "/kelas-5/debit-air/008-soal-matematika",
+    "Dera akan mengisi akuarium dengan air sebanyak 60000 cm<sup>3</sup>. Debit air dari siang 15 Liter/menit. Waktu yang dibutuhkan Dera untuk mengisi akuarium adalah ...",
+    ["Debit"],
+  ],
+  [
+    "Jan21_SM_09",
+    "/kelas-5/debit-air/009-soal-matematika",
+    "Sebuah pipa mengalirkan air sebanyak 2.4 m<sup>3</sup> selama 40 menit. Debit air tersebut adalah ... Liter/detik",
+    ["Debit"],
+  ],
+  [
+    "Jan21_SM_10",
+    "/kelas-5/debit-air/010-soal-matematika",
+    "Keran di rumah paman bocor. Tetesan air dari keran ditampung dalam ember. Debit air yang keluar dari keran adalah 70 cm<sup>3</sup>/menit. Volume air yang tertampung dalam ember selama 2.5 jam adalah ... Liter",
+    ["Debit"],
+  ],
+  [
+    "Jan21_SM_11",
+    "/kelas-5/skala/011-soal-matematika",
+    "panjang rel kereta api pada peta adalah 4 cm. Panjang sebenarnya rel kereta api adalah 2 km. Skala peta tersebut adalah ...",
+    ["Skala"],
+  ],
+  [
+    "Jan21_SM_12",
+    "/kelas-5/skala/012-soal-matematika",
+    "Jarak kota P ke kota Q pada peta adalah 12 cm. Jarak kota P ke kota Q sebenarnya adalah 72 km. Skala peta tersebut adalah ...",
+    ["Skala"],
+  ],
+  [
+    "Jan21_SM_13",
+    "/kelas-5/skala/013-soal-matematika",
+    "Panjang suatu jalan pada denah berskala 1 : 1400 adalah 4.5 cm. Panjang jalan sebenarnya adalah",
+    ["Skala"],
+  ],
+  [
+    "Jan21_SM_14",
+    "/kelas-5/skala/014-soal-matematika",
+    "Ayah memiliki kebun berbentuk persegi. Panjang sisi kebun pada denah adalah 10 cm. Skala denah 1 : 120. Luas kebun sebenarnya adalah ...",
+    ["Skala"],
+  ],
+  [
+    "Jan21_SM_15",
+    "/kelas-5/skala/015-soal-matematika",
+    "Jarak rumah Tino ke sekolah 3 km. Tino menggambar denah dari rumah ke sekolah dengan skala 1: 15000. Jarak rumah Tino ke sekolah pada denah adalah",
+    ["Skala"],
   ],
 ];
 
