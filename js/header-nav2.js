487f946f041cfd5a65d4979bfca5389afb722399
ocaz14 <nasri.anas14@gmail.com>
2021-01-14 23:08:35 +0800

    update

--- js/header-nav2.js
+++ js/header-nav2.js
@@ -4,8 +4,9 @@ header.innerHTML =
 "<div class='logo-container'>"+
 "   <div class='logo'>"+
 "       <a href='/index'><img src='/img/logo.png' alt=''></a>"+
+//"       <a href='/index'><img src='/img/logo3.png' alt=''></a>"+
 "   </div>"+
-"   <div class='logo-name'><a href='/index'>N Studio</a></div>"+
+"   <div class='logo-name'><a href='/index'>nasrianas.website</a></div>"+
 "</div>"+
 "<div class='navlinks-cont'>"+
 "   <a href='/index'><div class='nav-child'>Home</div></a>"+
