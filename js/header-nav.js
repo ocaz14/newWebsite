91ba45243fc8cd55a7d9a12b7498afd6f4d57d75
ocaz14 <nasri.anas14@gmail.com>
2021-02-21 13:00:25 +0800

    up

--- js/header-nav.js
+++ js/header-nav.js
@@ -1,33 +1,34 @@
 var header_nav =
   "<div class='header'>" +
-  "<div class='logo-container'>" +
-  "<div class='logo'>" +
-  "<a href='/index'><img src='/img/logo.png' alt=''></a>" +
-  "</div>" +
-  "</div>" +
-  "<div class='empty'></div>" +
-  "<ul class='nav-links'>" +
-  "<li class='project'>" +
-  "<a href='/project'>Project</a>" +
-  "</li>" +
-  "<li class='coding'>" +
-  "<a href='/coding'>Coding</a>" +
-  "</li>" +
-  "<li class='blog'>" +
-  "<a href='/blog'>Blog</a>" +
-  "</li>" +
-  "<li class='games'>" +
-  "<a href='/game'>Game</a>" +
-  "</li>" +
-  // "<li class='contact'>" +
-  // "<a href='#'>Contact</a>" +
-  // "</li>" +
-  "</ul>" +
-  "<div class='burger' id='burger'>" +
-  "<div class='line1'></div>" +
-  "<div class='line2'></div>" +
-  "<div class='line3'></div>" +
-  "</div>" +
+    "<div class='logo-container'>" +
+      "<div class='logo'>" +
+        "<a href='/index'><img src='/img/logo.png' alt=''></a>" +
+      "</div>" +
+    "</div>" +
+    "<div class='empty'></div>" +
+      "<ul class='nav-links'>" +
+        "<li class='project'>" +
+          "<a href='/project'>Project</a>" +
+        "</li>" +
+        "<li class='coding'>" +
+          "<a href='/coding'>Coding</a>" +
+        "</li>" +
+        "<li class='blog'>" +
+          "<a href='/blog'>Blog</a>" +
+        "</li>" +
+        "<li class='games'>" +
+          "<a href='/game'>Game</a>" +
+        "</li>" +
+        // "<li class='contact'>" +
+        // "<a href='#'>Contact</a>" +
+        // "</li>" +
+      "</ul>" +
+      "<div class='burger' id='burger'>" +
+        "<div class='line1'></div>" +
+        "<div class='line2'></div>" +
+        "<div class='line3'></div>" +
+      "</div>" +
+    "</div>" +
   "</div>";
 
 document.getElementById("header-nav").innerHTML = header_nav;
