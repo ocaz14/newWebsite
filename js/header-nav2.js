45393cdf76fa59a45db52d97b3d8a82ad0997d6f
ocaz14 <nasri.anas14@gmail.com>
2021-02-24 20:54:46 +0800

    up

--- js/header-nav2.js
+++ js/header-nav2.js
@@ -11,6 +11,7 @@ header.innerHTML =
 "<div class='navlinks-cont'>"+
 "   <a href='/index'><div class='nav-child'>Home</div></a>"+
 "   <a href='/blog'><div class='nav-child'>Blog</div></a>"+
+"   <a href='/coding'><div class='nav-child'>Coding</div></a>"+
 "   <a href='/project'><div class='nav-child'>Project</div></a>"+
 "</div>";
 
