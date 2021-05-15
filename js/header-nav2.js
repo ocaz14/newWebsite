7291c999a9b54119dfbc9bd8d01e8bafa2ae1007
ocaz14 <nasri.anas14@gmail.com>
2021-05-15 12:10:33 +0800

    up

--- js/header-nav2.js
+++ js/header-nav2.js
@@ -7,13 +7,16 @@ header.innerHTML =
 //"       <a href='/index'><img src='/img/logo3.png' alt=''></a>"+
 "   </div>"+
 "   <div class='logo-name'><a href='/index'>nasrianas.website</a></div>"+
-"</div>"+
+"</div>"
++
 "<div class='navlinks-cont'>"+
 "   <a href='/index'><div class='nav-child'>Home</div></a>"+
 "   <a href='/blog'><div class='nav-child'>Blog</div></a>"+
 "   <a href='/coding'><div class='nav-child'>Coding</div></a>"+
 "   <a href='/project'><div class='nav-child'>Project</div></a>"+
-"</div>";
+"</div>"
+;
+
 
 var themeColor = document.getElementById("theme-color");
 if(themeColor){
