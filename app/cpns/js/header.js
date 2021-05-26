4365bffe5dde55cdf9371406766c221124e7646c
ocaz14 <nasri.anas14@gmail.com>
2021-05-26 14:34:46 +0800

    up

---
+++ app/cpns/js/header.js
@@ -0,0 +1,6 @@
+var idHeader = document.getElementById("header")
+idHeader.innerHTML = 
+"<a href='/app/cpns'><div class='header-child'>TIU CPNS</div></a>"+
+"<a href='/app/cpns/sinonim'><div class='header-child'>Sinonim</div></a>"+
+"<a href='/app/cpns/antonim'><div class='header-child'>Antonim</div></a>"
+;
