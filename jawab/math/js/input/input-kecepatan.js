e288062fc162052ab8deda657e6362e3e2742b88
ocaz14 <nasri.anas14@gmail.com>
2021-09-06 09:37:38 +0800

    update panjang

---
+++ jawab/math/js/input/input-kecepatan.js
@@ -0,0 +1,20 @@
+var titleCat_1 = "Kecepatan";
+var cat_1 = [
+  ["meter/detik berapa km/jam", "/meter-per-detik-berapa-km-per-jam/001-1-meter-per-detik-berapa-km-per-jam", ""],
+
+];
+
+var url_root = "https://nasrianas.website/jawab/math/kecepatan";
+var footerArray = [
+    //June2020
+    [
+      "kecepatan_001",
+      "/meter-per-detik-berapa-km-per-jam/001-1-meter-per-detik-berapa-km-per-jam",
+      "1 m/s berapa km/jam ?",
+      "",
+      "1 m/s berapa km/jam beserta penjelasannya",
+      ["meter/detik berapa km/jam"],
+      "",
+    ],
+
+];
