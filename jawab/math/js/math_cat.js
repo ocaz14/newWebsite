39dead576625e5f8c93f9624ad2562754b8340a8
ocaz14 <nasri.anas14@gmail.com>
2021-11-07 12:20:40 +0800

    update berat

--- jawab/math/js/math_cat.js
+++ jawab/math/js/math_cat.js
@@ -1,11 +1,16 @@
 
 var titleCat_2 = "Math";
 var cat_2 = [
+  ["Berat", "/kg-ton/100-1-kg-berapa-ton", ""],
   ["Bilangan", "/bilangan/faktorisasi-prima/001-faktorisasi-prima-dari-80", ""],
+  ["Kecepatan", "/meter-per-detik-berapa-km-per-jam/001-1-meter-per-detik-berapa-km-per-jam", ""],
   ["Pangkat", "/pangkat/akar-pangkat3/057-akar-pangkat-3-dari-125000", ""],
   ["Panjang", "/panjang/meter-cm/001-1-meter-berapa-cm", ""],
   ["Pecahan", "/pecahan/decimal-pecahanBiasa/001-0,75-diubah-menjadi-pecahan-biasa", ""],
   ["Persen", "/persen/1persen/001-1-persen-dari-1-juta", ""],  
   ["Suhu", "/suhu/celcius-fahrenheit/001-30-derajat-celcius-berapa-fahrenheit", ""],
   ["Waktu", "/waktu/detik-menit/001-300-detik-berapa-menit", ""],
+
+  
+  
 ];
