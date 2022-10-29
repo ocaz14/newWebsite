de260932e4952b990fc2072d81f0ecab73a1385c
ocaz14 <nasri.anas14@gmail.com>
2022-10-29 16:13:12 +0800

    update sitemap berat

--- jawab/math/js/input/_cat/cat-berat.js
+++ jawab/math/js/input/_cat/cat-berat.js
@@ -1,5 +1,8 @@
 var cat_1 = [
   ["berapa", "/_berapa/1219-3-ton-2-kuintal-berapa-kg", ""],
+  ["gram-hg", "/gram-hg/4364-1-gram-berapa-hg", ""],
+  ["gram-kg", "/gram-kg/2775-0,001-gram-berapa-kg", ""],
+  ["gram-mg", "/gram-mg/4875-0,001-gram-berapa-mg", ""],
   ["gram-ons", "/gram-ons/623-1-gram-berapa-ons", ""],
   ["hg-gram", "/hg-gram/1836-0,1-hg-berapa-gram", ""],
   ["kg-gram", "/kg-gram/2175-0,001-kg-berapa-gram", ""],
@@ -9,10 +12,12 @@ var cat_1 = [
   ["kg-ton", "/kg-ton/100-1-kg-berapa-ton", ""],
   ["kuintal-kg", "/kuintal-kg/273-0,15-kuintal-berapa-kg", ""],
   ["kuintal-ton", "/kuintal-ton/177-1-kuintal-berapa-ton", ""],
+  ["mg-gram", "/mg-gram/5375-0,001-mg-berapa-gram", ""],
   ["ons-gram", "/ons-gram/1534-seperempat-ons-berapa-gram", ""],
   ["ons-kg", "/ons-kg/1535-setengah-ons-berapa-kg", ""],
   ["ton-kg", "/ton-kg/001-0,06-ton-berapa-kg", ""],
   ["ton-kuintal", "/ton-kuintal/224-0,08-ton-berapa-kuintal", ""],
+  ["ton-kuintal-berapa-kg", "/ton-kuintal-berapa-kg/0001-1-ton-1-kuintal-berapa-kg", ""],
 
 
 
