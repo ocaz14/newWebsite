6396405cf7eafa6be05877bed64f1c1c3fd7d3b9
ocaz14 <nasri.anas14@gmail.com>
2025-06-27 11:26:40 +0800

    update habit

--- app/habits/script.js
+++
@@ -1,5 +0,0 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/app/habits/service-worker.js')
-    .then(() => console.log("Service Worker registered"))
-    .catch(err => console.error("Service Worker registration failed:", err));
-}
