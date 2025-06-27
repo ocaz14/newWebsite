e07abc68c83c0dcd0303205d7deea045e482c3bd
ocaz14 <nasri.anas14@gmail.com>
2025-06-27 11:14:46 +0800

    udpate habit

--- app/habits/script.js
+++ app/habits/script.js
@@ -1,3 +1,5 @@
-
-// JavaScript logic here (same as previously provided)
-// Includes service worker registration at the end
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/app/habits/service-worker.js')
+    .then(() => console.log("Service Worker registered"))
+    .catch(err => console.error("Service Worker registration failed:", err));
+}
